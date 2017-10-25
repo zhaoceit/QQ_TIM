4bec48c0916eba779bb115b7284a35cfab418fc0
zhaoce <721875465@qq.com>
2017-10-25 20:03:39 +0800

    z

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zhousg on 2017/10/9.
+ */
+$(function () {
+    $.stellar({
+        /*横向五视差滚动效果*/
+        horizontalScrolling: false,
+        /*页面尺寸发生改变重新渲染页面*/
+        responsive: true
+    });
+});
